baf89c06b27fd4002c8d3fe6937c9bc006b4ca77
Yubisel Vega <yubiselv@gmail.com>
2024-01-24 15:05:05 -0300

    add table example with pdfkit-table

--- src/libs/pdfkit.js
+++ src/libs/pdfkit.js
@@ -1,4 +1,5 @@
-import PDFDocument from 'pdfkit';
+// import PDFDocument from 'pdfkit';
+import PDFDocument from 'pdfkit-table';
 
 function buildPDF(dataCallback, endCallback) {
   const doc = new PDFDocument();
@@ -8,6 +9,20 @@ function buildPDF(dataCallback, endCallback) {
   doc.on('end', endCallback);
   
   doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+
+  // -----------------------------------------------------------------------------------------------------
+  // Simple Table with Array
+  // -----------------------------------------------------------------------------------------------------
+  const tableArray = {
+    headers: ["Country", "Conversion rate", "Trend"],
+    rows: [
+      ["Switzerland", "12%", "+1.12%"],
+      ["France", "67%", "-0.98%"],
+      ["England", "33%", "+4.44%"],
+    ],
+  };
+  doc.table( tableArray, { width: 300 }); // A4 595.28 x 841.89 (portrait) (about width sizes)
+
   doc.end('end', endCallback);
 }
 
